1ccd25c7d85600f81bf1bd88c6d75cb936eb5530
xas-111 <gdesi2022@163.com>
2023-06-03 20:40:53 -0700

    Set initialized page to login

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <!-- element-ui installation test -->
     <!-- <element-view></element-view> -->
-    <login-view></login-view>
+    <!-- <login-view></login-view> -->
+
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import LoginView from './views/login/LoginView.vue'
+// import LoginView from './views/login/LoginView.vue'
 // import ElementView from './views/element/ElementView.vue'
 export default {
-  components: {LoginView /* ElementView */ },
+  components: {/* LoginView */ /* ElementView */ },
 
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/LoginView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/home/HomeView.vue')
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
 ]
 
 const router = new VueRouter({
